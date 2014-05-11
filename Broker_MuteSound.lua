c50896d7a46a90f38e2db80d228715cc0e9b8804
axiomasham <axio@axio.name>
2014-05-11 09:09:55 +0000

    Package-as Broker_MuteSound

---
+++ Broker_MuteSound.lua
@@ -0,0 +1,21 @@
+local LDB = LibStub("LibDataBroker-1.1"):NewDataObject("Broker_Mute", {
+	type = "data source",
+	text = "",
+	icon = (GetCVarBool("Sound_EnableAllSound") == nil and "Interface\\AddOns\\Broker_Mute\\Media\\Muted.tga") or "Interface\\AddOns\\Broker_Mute\\Media\\NotMuted.tga",
+	tocname = "Broker_Mute"}
+);
+LDB.OnTooltipShow = function(tooltip)
+	-- // todo
+	tooltip:AddLine("Broker: MuteSound");
+	tooltip:AddLine("|cffeda55fLeftClick:|r mute/unmute sound");
+	-- tooltip:AddLine("|cffeda55fRightClick:|r show options");
+end
+LDB.OnClick = function(displayFrame, button)
+	if (GetCVarBool("Sound_EnableAllSound") == nil) then
+		SetCVar("Sound_EnableAllSound", 1, true);
+		LDB.icon = "Interface\\AddOns\\Broker_Mute\\Media\\NotMuted.tga";
+	else
+		SetCVar("Sound_EnableAllSound", nil, true);
+		LDB.icon = "Interface\\AddOns\\Broker_Mute\\Media\\Muted.tga";
+	end
+end
