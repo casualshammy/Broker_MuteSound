582a3b0420478a41093498e2e26792c08391f1d8
axiomasham <axiomasham@fc2f0d5d-e619-4456-bd74-cf7df274e3b3>
2014-09-20 12:34:59 +0000

    Adapted for WoD

--- trunk/Broker_MuteSound.lua
+++ trunk/Broker_MuteSound.lua
@@ -1,21 +1,23 @@
 local LDB = LibStub("LibDataBroker-1.1"):NewDataObject("Broker_MuteSound", {
 	type = "data source",
 	text = "",
-	icon = (GetCVarBool("Sound_EnableAllSound") == nil and "Interface\\AddOns\\Broker_MuteSound\\Media\\Muted.tga") or "Interface\\AddOns\\Broker_MuteSound\\Media\\NotMuted.tga",
+	icon = (not GetCVarBool("Sound_EnableAllSound") and "Interface\\AddOns\\Broker_MuteSound\\Media\\Muted.tga") or "Interface\\AddOns\\Broker_MuteSound\\Media\\NotMuted.tga",
 	tocname = "Broker_MuteSound"}
 );
+
 LDB.OnTooltipShow = function(tooltip)
 	-- // todo
 	tooltip:AddLine("Broker: MuteSound");
 	tooltip:AddLine("|cffeda55fLeftClick:|r mute/unmute sound");
 	-- tooltip:AddLine("|cffeda55fRightClick:|r show options");
 end
+
 LDB.OnClick = function(displayFrame, button)
-	if (GetCVarBool("Sound_EnableAllSound") == nil) then
+	if (not GetCVarBool("Sound_EnableAllSound")) then
 		SetCVar("Sound_EnableAllSound", 1, true);
 		LDB.icon = "Interface\\AddOns\\Broker_MuteSound\\Media\\NotMuted.tga";
 	else
-		SetCVar("Sound_EnableAllSound", nil, true);
+		SetCVar("Sound_EnableAllSound", false, true);
 		LDB.icon = "Interface\\AddOns\\Broker_MuteSound\\Media\\Muted.tga";
 	end
 end
