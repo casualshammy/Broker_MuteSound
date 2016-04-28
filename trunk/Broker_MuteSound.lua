49d24f6610247c5cb8e187ce53a1a80e1cbc9506
axiomasham <axiomasham@fc2f0d5d-e619-4456-bd74-cf7df274e3b3>
2016-04-28 16:30:05 +0000

    Fixed a bug with icon causing it don't update if sound is muted via settings dialog

--- trunk/Broker_MuteSound.lua
+++ trunk/Broker_MuteSound.lua
@@ -1,23 +1,39 @@
+local tFrame = CreateFrame("frame");
+tFrame.elapsed = 0;
+
 local LDB = LibStub("LibDataBroker-1.1"):NewDataObject("Broker_MuteSound", {
 	type = "data source",
 	text = "",
-	icon = (not GetCVarBool("Sound_EnableAllSound") and "Interface\\AddOns\\Broker_MuteSound\\Media\\Muted.tga") or "Interface\\AddOns\\Broker_MuteSound\\Media\\NotMuted.tga",
+	icon = "Interface\\AddOns\\Broker_MuteSound\\Media\\NotMuted.tga",
 	tocname = "Broker_MuteSound"}
 );
 
 LDB.OnTooltipShow = function(tooltip)
-	-- // todo
 	tooltip:AddLine("Broker: MuteSound");
 	tooltip:AddLine("|cffeda55fLeftClick:|r mute/unmute sound");
-	-- tooltip:AddLine("|cffeda55fRightClick:|r show options");
+end
+
+local function Timer_OnElapsed()
+	if (not GetCVarBool("Sound_EnableAllSound")) then
+		LDB.icon = "Interface\\AddOns\\Broker_MuteSound\\Media\\Muted.tga";
+	else
+		LDB.icon = "Interface\\AddOns\\Broker_MuteSound\\Media\\NotMuted.tga";
+	end
 end
 
 LDB.OnClick = function(displayFrame, button)
 	if (not GetCVarBool("Sound_EnableAllSound")) then
 		SetCVar("Sound_EnableAllSound", 1, true);
-		LDB.icon = "Interface\\AddOns\\Broker_MuteSound\\Media\\NotMuted.tga";
 	else
 		SetCVar("Sound_EnableAllSound", false, true);
-		LDB.icon = "Interface\\AddOns\\Broker_MuteSound\\Media\\Muted.tga";
 	end
+	Timer_OnElapsed();
 end
+
+tFrame:SetScript("OnUpdate", function(this, elapsed)
+	this.elapsed = this.elapsed + elapsed;
+	if (this.elapsed >= 1.0) then
+		Timer_OnElapsed();
+		this.elapsed = 0;
+	end
+end);
